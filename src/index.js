e72c2b2e6f7ac091d26001053ea65c36359545f7
Marcelo Morgado <cmarcelom@gmail.com>
2025-04-02 11:28:53 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+var http = require("http");
+var express = require("express")();
+var cors = require("cors");
+var dotenv = require("dotenv");
+var axios = require("axios");
+var bodyParser = require("body-parser");
+var fastify = require("fastify")({ logger: true });
+var proxy = require("@fastify/http-proxy");
+
+dotenv.config();
+
+const { API_HTTP_URL, CORS_ORIGIN, HTTP_PORT, WS_PORT } = process.env;
+
+express.use(bodyParser.json());
+express.use(cors({ origin: CORS_ORIGIN }));
+
+const server = http.createServer(express);
+
+express.post("/", async function (req, res) {
+  try {
+    const response = await axios.post(API_HTTP_URL, {
+      ...req.body,
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    res.status(500).send("Error fetching data");
+  }
+});
+
+fastify.register(proxy, {
+  upstream: API_HTTP_URL,
+  prefix: "/",
+  websocket: true,
+});
+
+fastify.listen({ port: WS_PORT });
+server.listen(HTTP_PORT);
